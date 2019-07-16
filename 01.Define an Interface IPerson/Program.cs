721b1a93ba79c4efec392dc0ca1405598e8b325f
Angel Hristov <angelxristov84@abv.bg>
2019-07-16 21:04:28 +0300

    Add project files.

---
+++ 01.Define an Interface IPerson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Define_an_Interface_IPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
